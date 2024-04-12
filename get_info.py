584631a1d5f24f310635646f1bc4e10a20824030
Arka Dash <dasharka05@gmail.com>
2024-04-13 00:03:19 +0530

    simplified

--- get_info.py
+++ get_info.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
-import re
 import openfoodfacts
-from simplify import simplify
+from simplify import nutri_simplify
 
 
 app = Flask(__name__)
@@ -13,7 +12,10 @@ def get_product_info():
     api = openfoodfacts.API(user_agent="MyAwesomeApp/1.0")
     product_info = api.product.get(code, fields=["code", "product_name", "image_url", "brands", "brands_tags", "nutriscore_data", "nutriments"])
     
-    product_info['nutriments'] = simplify(product_info, product_info['nutriments'])
+    # simplify the the nutrients
+    nutri_simplify(product_info, product_info['nutriments'])
+    
+    nutri_simplify(product_info, product_info['nutriscore_data'])
     
     return jsonify(product_info)
     
--- simplify.py
+++ simplify.py
@@ -1,10 +1,22 @@
 import re
 
-def simplify(container, facts):
-    dct= {}
-    for val in facts:
-        if len(re.split(r"[_-]", val)) == 1:
-            dct[val] = facts[val]
-        if len(re.split(r"[_-]", val)) == 2 and val.split('_')[-1] == 'unit':
-            dct[val] = facts[val]
-    return dct
+def nutri_simplify(container, facts):
+    if facts == container['nutriments']:
+        dct= {}
+        for val in facts:
+            if len(re.split(r"[_-]", val)) == 1:
+                dct[val] = facts[val]
+                perv = val
+            if len(re.split(r"[_-]", val)) == 2 and val.split('_')[-1] == 'unit':
+                dct[perv] = str(f"{dct[perv]} ") + facts[val] 
+        
+        dct['energy'] = str(round(int(dct['energy'].split()[0]) * 0.239006)) + " Kcal"
+        container['nutriments'] = dct
+        
+    elif facts == container["nutriscore_data"]:
+        dct = {}
+        for key in facts:
+            if len(key.split('_')) != 1 and key != 'grade':
+                dct[key] = facts[key]
+        container["nutriscore_data"] = dct
+    
